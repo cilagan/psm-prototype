c9291e135cf18c9c8ac983ac18656a03b24bb28f
cilagan <cilagan@DIS-CILAGAN-01.ad.nsf.gov>
2015-11-24 10:50:19 -0500

    Added controller methods. Cleaned up

--- document-service/src/main/java/gov/nsf/research/document/service/controller/DocumentServiceController.java
+++ document-service/src/main/java/gov/nsf/research/document/service/controller/DocumentServiceController.java
@@ -4,7 +4,10 @@ import java.util.Date;
 
 import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gov.nsf.research.document.service.model.proposal.DataMgtPlan;
@@ -12,6 +15,7 @@ import gov.nsf.research.document.service.model.proposal.ProjectDesc;
 import gov.nsf.research.document.service.model.proposal.Proposal;
 
 @RestController
+@RequestMapping(path="/docService")
 public class DocumentServiceController {
 	
 	GridFsTemplate gfsTemplate;
@@ -41,5 +45,29 @@ public class DocumentServiceController {
 		proposal.setDataMgtPlan(dmp);
 		
 		return proposal;
+	}
+	
+	@RequestMapping(path="/proposal/{tempPropId}/projdesc", method = RequestMethod.POST)
+	public void uploadProjSumm(@PathVariable String tempPropId){
+	}
+	
+	@RequestMapping(path="/proposal/{tempPropId}/projdesc", method = RequestMethod.GET)
+	public ProjectDesc getProjectDesc(@PathVariable String tempPropId){
+		return new ProjectDesc();
+	}
+	
+	@RequestMapping(path="/proposal/{tempPropId}/dmp", method = RequestMethod.POST)
+	public void uploadDMP(@PathVariable String tempPropId){
+		
+	}
+	
+	@RequestMapping(path="/proposal/{tempPropId}/dmp", method = RequestMethod.GET)
+	public DataMgtPlan getDMP(@PathVariable String tempPropId){
+		return new DataMgtPlan();
+	}
+	
+	@RequestMapping(path="/proposal/{tempPropId}", method = RequestMethod.GET)
+	public Proposal getEntireProposal(@PathVariable String tempPropId){
+		return new Proposal();
 	}
 }
