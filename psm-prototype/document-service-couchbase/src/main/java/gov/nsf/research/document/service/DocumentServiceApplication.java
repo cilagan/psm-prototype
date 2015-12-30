417976de82e423e762e7bfd97a651005d1b45ec4
unknown <cilagan@DIS-CILAGAN-01.ad.nsf.gov>
2015-12-30 16:26:52 -0500

    Updated Config

--- psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/DocumentServiceApplication.java
+++ psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/DocumentServiceApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableScheduling
+@EnableCaching
 public class DocumentServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
