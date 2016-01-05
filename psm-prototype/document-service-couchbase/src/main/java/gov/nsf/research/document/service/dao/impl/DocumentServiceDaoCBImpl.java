bb2a89b7a77760c88b2395292c263c17ce80d6bd
unknown <spendyal@DIS-SPENDYAL-1.ad.nsf.gov>
2016-01-05 15:08:07 -0500

    Implemented SaveDocument and viewDocument methods

--- psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/impl/DocumentServiceDaoCBImpl.java
+++ psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/impl/DocumentServiceDaoCBImpl.java
@@ -1,16 +1,24 @@
 package gov.nsf.research.document.service.dao.impl;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.util.List;
-
-import org.springframework.data.couchbase.core.CouchbaseTemplate;
-
 import gov.nsf.research.document.service.dao.DocumentServiceDao;
 import gov.nsf.research.document.service.model.Document;
 import gov.nsf.research.document.service.model.DocumentMetaData;
 import gov.nsf.research.document.service.model.SectionType;
-import gov.nsf.research.document.service.model.document.User;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.data.couchbase.core.CouchbaseTemplate;
+
+import com.couchbase.client.CouchbaseClient;
 
 
 public class DocumentServiceDaoCBImpl implements  DocumentServiceDao {
@@ -24,18 +32,67 @@ public class DocumentServiceDaoCBImpl implements  DocumentServiceDao {
 
 
 	@Override
-	public DocumentMetaData saveDocument(InputStream inputStream, String tempPropId, SectionType sectionType) {
-		User user = new User("3", "Suresh", "Pendyala");
-		couchBaseTemplate.insert(user);
-		System.out.println(couchBaseTemplate.findById("3", User.class).toString());
+	public DocumentMetaData saveDocument(InputStream inputStream, String tempPropId,
+			SectionType sectionType) {
+
+		CouchbaseClient client = couchBaseTemplate.getCouchbaseClient();
+
+		try {
+
+			BufferedImage image = ImageIO.read(inputStream);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(image, "JPG", baos);
+			baos.flush();
+			byte[] bytes = baos.toByteArray();
+			baos.close();
+			try {
+				client.set(tempPropId, bytes).get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+
+		} catch (Exception e) {
+			e.toString();
+		}
+
 		return null;
 	}
-
+	
 
 	@Override
 	public ByteArrayOutputStream viewDocument(String tempPropId, SectionType sectionType) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		CouchbaseClient client = couchBaseTemplate.getCouchbaseClient();
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		
+		byte[] bytes = (byte[]) client.get(tempPropId);
+		InputStream in = new ByteArrayInputStream(bytes);
+		BufferedImage buffImage;
+		try {
+			buffImage = ImageIO.read(in);
+			ImageIO.write(
+					buffImage,
+					"JPG",
+					new File(
+							"C:\\Users\\spendyal\\Desktop\\psm_test_input_ files\\"+tempPropId +".JPG"));
+			
+			ImageIO.write(
+					buffImage,
+					"JPG",outputStream);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		return outputStream;
 	}
 
 
