1e50b754cfb09f420a98006178e5caa3d27a281b
571925 <chelseashorte@C02QK0RLG8WM.local>
2016-01-06 11:13:54 -0500

    Updates with buildURL following the server API

---
+++ psm-prototype/psm-prototype-front-end/app/adapters/proposal/cover-sheet.js
@@ -0,0 +1,19 @@
+import DS from 'ember-data';
+
+export default DS.RESTAdapter.extend( {
+  namespace: 'docService',
+
+  buildURL: function (modelName, id, snapshot, requestType, query) {
+    var url = this._super();
+    switch (requestType) {
+      case 'deleteRecord':
+        url += '/delete';
+      case 'createRecord':
+      case 'findRecord':
+      case 'updateRecord':
+        url += '/proposal/'+id+'/cover-sheet';
+        return url;
+    }
+  }
+
+});
