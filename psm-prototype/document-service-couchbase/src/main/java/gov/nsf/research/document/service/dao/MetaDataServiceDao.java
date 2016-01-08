77454e0170e5b55ece25493ec5e37f8955275954
unknown <spendyal@DIS-SPENDYAL-1.ad.nsf.gov>
2016-01-08 16:23:40 -0500

    Implemented UploadFile

---
+++ psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/MetaDataServiceDao.java
@@ -0,0 +1,27 @@
+package gov.nsf.research.document.service.dao;
+
+import gov.nsf.research.document.service.model.DocumentMetaData;
+
+public interface MetaDataServiceDao {
+	
+	/**
+	 * 
+	 * @param documentMetaData
+	 */
+	public void saveDocumentMetaData(DocumentMetaData documentMetaData);
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public DocumentMetaData viewDocumentMetaData(String key);
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean deleteDocumentMetaData(String key);
+
+}
--- psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/impl/FileStoreDaoAmazonS3Impl.java
+++ psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/impl/FileStoreDaoAmazonS3Impl.java
@@ -1,22 +1,26 @@
 package gov.nsf.research.document.service.dao.impl;
 
+import gov.nsf.research.document.service.dao.FileStoreDao;
+import gov.nsf.research.document.service.dao.MetaDataServiceDao;
+
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-
-import gov.nsf.research.document.service.dao.FileStoreDao;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 public class FileStoreDaoAmazonS3Impl implements FileStoreDao {
+	
+	private static final String BUCKET_NAME = "psm_data_store_01";
 
 	@Autowired
 	AmazonS3 amazonS3;
+	
+	@Autowired
+	MetaDataServiceDao metaDataServiceDao;
 
 	@Override
 	public boolean checkFileExist(String fileName) {
@@ -26,9 +30,13 @@ public class FileStoreDaoAmazonS3Impl implements FileStoreDao {
 
 	@Override
 	public boolean uploadFile(InputStream inputStream, String fileName) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		amazonS3.putObject(new PutObjectRequest(BUCKET_NAME, fileName,
+				inputStream, new ObjectMetadata()));
+
+		return true;
 	}
+
 
 	@Override
 	public boolean deleteFile(String fileName) {
