284adb48d0656fa13e733c9e9c5534b06c32dc15
unknown <pkatrapa@DIS-PKATRAPA.ad.nsf.gov>
2016-01-10 16:15:39 -0500

    test cases added for deleteFile, checkcFileExist and downloadFile

--- psm-prototype/document-service-couchbase/src/test/java/gov/nsf/research/document/service/dao/impl/FileStoreDaoAmazonS3ImplTest.java
+++ psm-prototype/document-service-couchbase/src/test/java/gov/nsf/research/document/service/dao/impl/FileStoreDaoAmazonS3ImplTest.java
@@ -1,10 +1,9 @@
 package gov.nsf.research.document.service.dao.impl;
 
-import gov.nsf.research.document.service.DocumentServiceApplication;
-import gov.nsf.research.document.service.dao.FileStoreDao;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,12 +11,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.StringUtils;
+
+import gov.nsf.research.document.service.DocumentServiceApplication;
+import gov.nsf.research.document.service.dao.FileStoreDao;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DocumentServiceApplication.class)
 public class FileStoreDaoAmazonS3ImplTest {
 
 	@Autowired
 	FileStoreDao fileStoreDao;
+	
+	@Autowired
+	private AmazonS3 amazonS3;
+	
 	
 	@Test
 	public void contextLoads() {
@@ -39,6 +52,51 @@ public class FileStoreDaoAmazonS3ImplTest {
 		
 		fileStoreDao.uploadFile(inputStream, tempPropId);
 
+	}
+	
+	@Test
+	public void testDownloadFile(){
+		System.out.println("DocumentServiceApplicationTests.testDownloadFile() Downloading File Content :");
+		fileStoreDao.downloadFile("C:\\TestUploads\\Upload.txt");
+		
+	}
+	
+	@Test
+	public void tesDeleteFile(){
+		System.out.println("DocumentServiceApplicationTests.testDownloadFile() deleting File");
+		fileStoreDao.deleteFile("C:\\TestUploads\\Upload.txt");
+		
+	}
+	
+	
+	@Test
+	public void tesFileExist(){
+		System.out.println("DocumentServiceApplicationTests.testDownloadFile() deleting File");
+		String fileName = "12345689";
+		boolean fileExist = fileStoreDao.checkFileExist(fileName);
+		System.out.println("DocumentServiceApplicationTests.tesFileExist() -- ["+fileName+"] : "+fileExist);
+		
+	}
+	
+	@Test
+	public void testAWSS3(){
+		AmazonS3 conn = amazonS3;
+		
+		List<Bucket> buckets = conn.listBuckets();
+		for (Bucket bucket : buckets) {
+		        System.out.println(bucket.getName() + "\t" + StringUtils.fromDate(bucket.getCreationDate()));
+		}
+        System.out.println("Listing objects");
+        ObjectListing objectListing = amazonS3.listObjects(new ListObjectsRequest().withBucketName("psm-data-store-01"));
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            System.out.println(" Key - " + objectSummary.getKey() + "  " +
+                    "(size = " + objectSummary.getSize() + ")");
+        }
+        
+        System.out.println();
+		
+//		ByteArrayInputStream input = new ByteArrayInputStream("Hello World!".getBytes());
+//		conn.putObject("psm-data-store", "hello.txt", input, new ObjectMetadata());
 	}
 	
 }
