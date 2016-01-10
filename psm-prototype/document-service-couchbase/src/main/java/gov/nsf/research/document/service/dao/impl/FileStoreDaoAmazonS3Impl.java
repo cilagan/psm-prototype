2295f0fb0bd0afdc2a4acfe529c7297e4403e900
unknown <pkatrapa@DIS-PKATRAPA.ad.nsf.gov>
2016-01-10 16:07:08 -0500

    methods implemented

--- psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/impl/FileStoreDaoAmazonS3Impl.java
+++ psm-prototype/document-service-couchbase/src/main/java/gov/nsf/research/document/service/dao/impl/FileStoreDaoAmazonS3Impl.java
@@ -1,16 +1,27 @@
 package gov.nsf.research.document.service.dao.impl;
 
-import gov.nsf.research.document.service.dao.FileStoreDao;
-import gov.nsf.research.document.service.dao.MetaDataServiceDao;
-
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import gov.nsf.research.document.service.dao.FileStoreDao;
+import gov.nsf.research.document.service.dao.MetaDataServiceDao;
 
 public class FileStoreDaoAmazonS3Impl implements FileStoreDao {
 	
@@ -24,8 +35,38 @@ public class FileStoreDaoAmazonS3Impl implements FileStoreDao {
 
 	@Override
 	public boolean checkFileExist(String fileName) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean fileExist = false;
+
+		try {
+			ObjectListing objectListing = amazonS3.listObjects(new ListObjectsRequest().withBucketName(BUCKET_NAME));
+			for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+				String key = objectSummary.getKey();
+				System.out.println(" Key - " + key);
+				if(fileName.equals(key)){
+					fileExist = true;
+					break;
+				}
+			}
+		}  catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException, which " +
+					"means your request made it " +
+					"to Amazon S3, but was rejected with an error response" +
+					" for some reason.");
+			System.out.println("Error Message: " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code: " + ase.getErrorCode());
+			System.out.println("Error Type: " + ase.getErrorType());
+			System.out.println("Request ID: " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, which " +
+					"means the client encountered " +
+					"an internal error while trying to " +
+					"communicate with S3, " +
+					"such as not being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}
+
+		return fileExist;
 	}
 
 	@Override
@@ -40,14 +81,79 @@ public class FileStoreDaoAmazonS3Impl implements FileStoreDao {
 
 	@Override
 	public boolean deleteFile(String fileName) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean deleted = false;
+
+		try {
+			amazonS3.deleteObject(new DeleteObjectRequest(BUCKET_NAME, fileName));
+
+			deleted = true;
+		} catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException, which " +
+					"means your request made it " +
+					"to Amazon S3, but was rejected with an error response" +
+					" for some reason.");
+			System.out.println("Error Message: " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code: " + ase.getErrorCode());
+			System.out.println("Error Type: " + ase.getErrorType());
+			System.out.println("Request ID: " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, which " +
+					"means the client encountered " +
+					"an internal error while trying to " +
+					"communicate with S3, " +
+					"such as not being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}
+
+		return deleted;
 	}
 
 	@Override
 	public OutputStream downloadFile(String fileName) {
 		// TODO Auto-generated method stub
+		// return null;
+		String bucketName = "";
+		try {
+			System.out.println("Downloading an object");
+			S3Object object = amazonS3.getObject(new GetObjectRequest(bucketName,fileName));
+			System.out.println("Content-Type: " + object.getObjectMetadata().getContentType());
+			displayTextInputStream(object.getObjectContent());
+		} catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException, which" +
+					" means your request made it " +
+					"to Amazon S3, but was rejected with an error response" +
+					" for some reason.");
+			System.out.println("Error Message: " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code: " + ase.getErrorCode());
+			System.out.println("Error Type: " + ase.getErrorType());
+			System.out.println("Request ID: " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, which means"+
+					" the client encountered " +
+					"an internal error while trying to " +
+					"communicate with S3, " +
+					"such as not being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return null;
+
 	}
+
+	private static void displayTextInputStream(InputStream input) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		while (true) {
+			String line = reader.readLine();
+			if (line == null)
+				break;
+
+			System.out.println("    " + line);
+		}
+		System.out.println();
+	}
+
 
 }
