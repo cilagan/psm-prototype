365dd56024e849f458c571daeb964931471e7abd
unknown <spendyal@DIS-SPENDYAL-1.ad.nsf.gov>
2016-02-11 12:10:53 -0500

    Added Storeproc class and mapper classes

---
+++ psm-prototype/proposal-management-service/src/main/java/gov/nsf/research/psm/storeprocedure/SPGetProgramElement.java
@@ -0,0 +1,56 @@
+package gov.nsf.research.psm.storeprocedure;
+
+import gov.nsf.research.psm.storeprocedure.mapper.ProgramElementMapper;
+
+import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.SqlReturnResultSet;
+import org.springframework.jdbc.object.StoredProcedure;
+
+public class SPGetProgramElement extends StoredProcedure {
+
+	public static final String STORED_PROC_GET_PROGRAMELEMENT_LIST = "flp.pr_psm_sel_pgm_ele";
+	public static final String RESULT_SET = "ProgramElement";
+	private static final String IN_PGM_ANNC_ID = "pgmAnncID";
+	private static final String IN_DIV_CODE = "divCode";
+
+	public SPGetProgramElement() {
+		super();
+	}
+
+	public SPGetProgramElement(DataSource dataSource, String storedProcName) {
+		super(dataSource, storedProcName);
+
+		declareParameter(new SqlReturnResultSet(RESULT_SET,
+				new ProgramElementMapper()));
+		declareParameter(new SqlParameter(SPGetProgramElement.IN_PGM_ANNC_ID,
+				Types.VARCHAR));
+		declareParameter(new SqlParameter(SPGetProgramElement.IN_DIV_CODE,
+				Types.VARCHAR));
+
+		compile();
+	}
+
+	public SPGetProgramElement(JdbcTemplate jdbcTemplate, String name) {
+		super(jdbcTemplate, name);
+	}
+
+	public final Map<String, Object> execute(String pgmAnncID, String divisionCode) {
+		Map<String, Object> inParams = new HashMap<String, Object>();
+
+		inParams.put(SPGetProgramElement.IN_PGM_ANNC_ID, pgmAnncID);
+		inParams.put(SPGetProgramElement.IN_DIV_CODE, divisionCode);
+
+		Map<String, Object> results = super.execute(inParams);
+
+		return results;
+	}
+
+
+}
