04cf6b0d3ec36d8d1dfeb7741ad88c35efaac95d
unknown <spendyal@DIS-SPENDYAL-1.ad.nsf.gov>
2016-04-22 09:55:26 -0400

    Project Summary Changes

--- psm-prototype/document-service-mongodb-itext/src/main/java/gov/nsf/research/document/service/dao/ProposalDao.java
+++ psm-prototype/document-service-mongodb-itext/src/main/java/gov/nsf/research/document/service/dao/ProposalDao.java
@@ -1,5 +1,7 @@
 package gov.nsf.research.document.service.dao;
 
+import gov.nsf.research.document.service.model.proposal.ProjectSummary;
+
 
 
 /**
@@ -8,12 +10,31 @@ package gov.nsf.research.document.service.dao;
  */
 public interface ProposalDao {
 
+	
 	/**
 	 * 
-	 * @return list of fundingopportunity
+	 * @param tempPropID
+	 * @return
 	 */
-	public String getProjectSummary(String tempPropID);
+	public ProjectSummary getProjectSummary(String tempPropID);
 	
+	
+	
+	/**
+	 * 
+	 * @param overView
+	 * @param intulMerit
+	 * @param brodrImpt
+	 * @return
+	 */
+	
+	public void saveProjectSummary(String tempPropID, String overView, String intulMerit, String brodrImpt);
+	
+	/**
+	 * 
+	 * @param tempPropID
+	 * @return
+	 */
 	public String getStampPDFTimeStamp(String tempPropID);
 
 	
