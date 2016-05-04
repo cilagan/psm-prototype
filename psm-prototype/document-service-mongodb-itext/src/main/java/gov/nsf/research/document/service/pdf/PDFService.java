c9c3f5dddbcf59ef8fc4a31703cbb74e22301e39
unknown <spendyal@DIS-SPENDYAL-1.ad.nsf.gov>
2016-05-04 11:16:40 -0400

    no message

--- psm-prototype/document-service-mongodb-itext/src/main/java/gov/nsf/research/document/service/pdf/PDFService.java
+++ psm-prototype/document-service-mongodb-itext/src/main/java/gov/nsf/research/document/service/pdf/PDFService.java
@@ -4,18 +4,23 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.itextpdf.text.pdf.PdfReader;
 
+import gov.nsf.research.document.service.model.EditorText;
 import gov.nsf.research.document.service.model.PDFDocument;
 import gov.nsf.research.document.service.model.SectionType;
+import gov.nsf.research.document.service.model.proposal.ProjectSummary;
 
 public interface PDFService {
 	
 	public PDFDocument validatePDFDocument(ByteArrayInputStream inputStream,
 			String tempPropId);
 
-	public ByteArrayOutputStream createPDF(String text);
+	public ByteArrayOutputStream createPDF(ProjectSummary text);
+	
+	public ByteArrayOutputStream createProjectSummaryPDF(Set<EditorText> overView, Set<EditorText> brodImpt, Set<EditorText> intellectual );
 
 	public ByteArrayOutputStream CreateEntireProposal(String tempPropId,
 			List<ByteArrayOutputStream> outputStreamList);
@@ -25,4 +30,6 @@ public interface PDFService {
 	public ByteArrayOutputStream CreateEntireProposalWithBookMarks(Map<SectionType, ByteArrayOutputStream> filesToMerge);
 
 	public ByteArrayOutputStream stampPDF(ByteArrayOutputStream srcDocStream, String sampText);
+	
+	public int pageCount( ByteArrayOutputStream baos);
 }
