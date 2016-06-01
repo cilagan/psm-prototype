79b3e1f02adcb0087acc43b6119881bc591230b9
unknown <spendyal@DIS-SPENDYAL-1.ad.nsf.gov>
2016-06-01 11:54:40 -0400

    no message

--- psm-prototype/document-generation-service/src/main/java/gov/nsf/research/document/service/pdf/impl/ITextPDFServiceImpl.java
+++ psm-prototype/document-generation-service/src/main/java/gov/nsf/research/document/service/pdf/impl/ITextPDFServiceImpl.java
@@ -2,17 +2,15 @@ package gov.nsf.research.document.service.pdf.impl;
 
 import gov.nsf.research.document.service.model.SectionType;
 import gov.nsf.research.document.service.pdf.PDFService;
+import gov.nsf.research.psm.proposal.transfer.proposals.BioSketches;
 import gov.nsf.research.psm.proposal.transfer.proposals.GetProposalResponse;
 import gov.nsf.research.psm.proposal.webservice.client.ProposalDataServiceClient;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.FileCopyUtils;
@@ -43,22 +41,38 @@ public class ITextPDFServiceImpl implements PDFService {
 	
 	
 	/**
+	 * NFS mount 
+	 */
+	public static final String NFS_MOUNT = "/mnt/pdfs";
+	
+	
+	/**
 	 * 
 	 */
 	@Override
 	public ByteArrayOutputStream createPDF(SectionType sectionType,
 			String tempPropId) {
-		System.out.println("ITextPDFServiceImpl.createPDF()");
+		System.out.println("ITextPDFServiceImpl.createPDF()[SectionType ="
+				+ sectionType + ":Temp Prop ID =" + tempPropId + "]");
 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
 		if (SectionType.PROJ_SUMM.equals(sectionType)) {
-			
+
 			baos = generateProjectSummaryPDF(tempPropId);
-			
+
 		} else if (SectionType.REF_CITED.equals(sectionType)) {
 
 			baos = generateRefCitedPDF(tempPropId);
+
+		} else if (SectionType.BIO_SKETCHES.equals(sectionType)) {
+
+			baos = generateBiographicalSketchesPDF(tempPropId);
+		}
+
+		else if (SectionType.FAC_EQUP.equals(sectionType)) {
+
+			baos = generateFacEquipmentPDF(tempPropId);
 		}
 
 		return baos;
@@ -69,7 +83,7 @@ public class ITextPDFServiceImpl implements PDFService {
 	@Override
 	public ByteArrayOutputStream CreateEntireProposalPDF(String tempPropId) {
 
-		System.out.println("ITextPDFServiceImpl.CreateEntireProposalPDF()");
+		System.out.println("ITextPDFServiceImpl.CreateEntireProposalPDF()[Temp Prop ID =" + tempPropId + "]");
 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		List<ByteArrayOutputStream> baosList = new ArrayList<ByteArrayOutputStream>();
@@ -77,21 +91,29 @@ public class ITextPDFServiceImpl implements PDFService {
 		//Project Summary
 		ByteArrayOutputStream projsumm = createPDF(SectionType.PROJ_SUMM, tempPropId);
 		GetProposalResponse proposal = proposalDataServiceClient.getProposal(tempPropId);
-		XMLGregorianCalendar xmlDate = proposal.getProposal().getProposalHeader().getSubmitTimeStamp();
-		java.util.Date date = xmlDate.toGregorianCalendar().getTime();
-		projsumm = stampPDF(projsumm, SectionType.PROJ_SUMM, "Corrected : "+new SimpleDateFormat("MM/dd/yyyy").format(date));
+		//XMLGregorianCalendar xmlDate = proposal.getProposal().getProposalHeader().getSubmitTimeStamp();
+		//java.util.Date date = xmlDate.toGregorianCalendar().getTime();
+		//projsumm = stampPDF(projsumm, SectionType.PROJ_SUMM, "Corrected : "+new SimpleDateFormat("MM/dd/yyyy").format(date));
 		
 		//Reference Cited
 		ByteArrayOutputStream refcited = createPDF(SectionType.REF_CITED,	tempPropId);
-//		String stampText ="PI Transfer/Award No:1100423/Submitted on:"+new SimpleDateFormat("MMMM dd yyyy hh:mm a").format(new Date())+" /Electronic Signature";
 		String stampText ="Place Holder for PI Transfer/Revised Budget PDF Stamp";
 		refcited = stampPDF(refcited, SectionType.REF_CITED,stampText);
 		
+		//Biographical Sketches
+		ByteArrayOutputStream bs = createPDF(SectionType.BIO_SKETCHES,	tempPropId);
+		
+		//Facilities Equipment
+		ByteArrayOutputStream fac = createPDF(SectionType.FAC_EQUP,	tempPropId);
+				
+				
 		//add Proposal Sections to the list
 		baosList.add(projsumm);
 		baosList.add(refcited);
+		baosList.add(bs);
+		baosList.add(fac);
 
-		//concatente PDF
+		//Concatenate PDF
 		baos = concatentePDF(baosList);
 
 		return baos;
@@ -99,26 +121,27 @@ public class ITextPDFServiceImpl implements PDFService {
 	}
 
 	
-
 	
 
 	private ByteArrayOutputStream generateProjectSummaryPDF(String tempPropId) {
 
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		
-		GetProposalResponse proposal = proposalDataServiceClient.getProposal(tempPropId);
-		
-		String filepath = proposal.getProposal().getProposalSections().getProjectSummary().getFilePath();
-		
-		//If file path is not empty , then read PDF from NFS mount
-		if (filepath != null) {		
-			filepath = "ps.pdf";
-			outputStream = getPdfFromNfsMount(filepath);	
-			
+
+		GetProposalResponse proposal = proposalDataServiceClient
+				.getProposal(tempPropId);
+
+		String filepath = proposal.getProposal().getProposalSections()
+				.getProjectSummary().getFilePath();
+		System.out.println("ITextPDFServiceImpl.generateProjectSummaryPDF()..Filepath="+filepath);
+
+		// If file path is not empty , then read PDF from NFS mount
+		if (filepath != null) {
+			filepath = NFS_MOUNT + filepath;
+			outputStream = getPdfFromNfsMount(filepath);
 			return outputStream;
 		}
 
-		//Read the text from database and generate PDF
+		// Read the text from database and generate PDF
 		// Project Summary template
 		Document document = new Document(PageSize.A4, 72, 72, 72, 72);
 
@@ -144,7 +167,7 @@ public class ITextPDFServiceImpl implements PDFService {
 			document.add(overViewParagraph);
 			document.add(new Paragraph(proposal.getProposal()
 					.getProposalSections().getProjectSummary()
-					.getProjSummaryText(),getTextFont()));
+					.getProjSummaryText(), getTextFont()));
 
 			// BrodrImpt Paragraph set up
 			brodrImptParagraph = new Paragraph("Broader Impacts :",
@@ -152,7 +175,7 @@ public class ITextPDFServiceImpl implements PDFService {
 			document.add(brodrImptParagraph);
 			document.add(new Paragraph(proposal.getProposal()
 					.getProposalSections().getProjectSummary()
-					.getBroaderImpacts(),getTextFont()));
+					.getBroaderImpacts(), getTextFont()));
 
 			// IntulMerit Paragraph set up
 			intulMeritParagraph = new Paragraph("Intellectual Merit :",
@@ -160,7 +183,7 @@ public class ITextPDFServiceImpl implements PDFService {
 			document.add(intulMeritParagraph);
 			document.add(new Paragraph(proposal.getProposal()
 					.getProposalSections().getProjectSummary()
-					.getIntellectualMerit(),getTextFont()));
+					.getIntellectualMerit(), getTextFont()));
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -177,23 +200,23 @@ public class ITextPDFServiceImpl implements PDFService {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		GetProposalResponse proposal = proposalDataServiceClient
 				.getProposal(tempPropId);
-		
+
 		String filepath = null;
-		
-		filepath =  proposal.getProposal()	.getProposalSections().getReferencesCited().getFilePath();
-		
-	
-		//If file path is not empty , then read PDF from NFS mount
-		if (filepath != null) {	
-			filepath = "rc.pdf";
-			outputStream = getPdfFromNfsMount(filepath);	
-			
+
+		filepath = proposal.getProposal().getProposalSections()
+				.getReferencesCited().getFilePath();
+		System.out.println("ITextPDFServiceImpl.generateRefCitedPDF()..filepath"+filepath);
+
+		// If file path is not empty , then read PDF from NFS mount
+		if (filepath != null) {
+			filepath = NFS_MOUNT + filepath;
+			outputStream = getPdfFromNfsMount(filepath);
+
 			return outputStream;
 		}
 
-		//Read the text from database and generate PDF
-		// Reference Cited template		
-		
+		// Read the text from database and generate PDF
+		// Reference Cited template
 
 		// page size
 		Document document = new Document(PageSize.A4, 72, 72, 72, 72);
@@ -212,15 +235,78 @@ public class ITextPDFServiceImpl implements PDFService {
 			Chunk linebreak = new Chunk(new LineSeparator());
 			document.add(linebreak);
 
-			
-			document.add(new Paragraph(proposal.getProposal().getProposalSections().getReferencesCited().getRefCitedTxt(), getTextFont()));
-			
+			document.add(new Paragraph(proposal.getProposal()
+					.getProposalSections().getReferencesCited()
+					.getRefCitedTxt(), getTextFont()));
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			document.close();
 		}
+		return outputStream;
+
+	}
+	
+	private ByteArrayOutputStream generateBiographicalSketchesPDF(String tempPropId) {
+		System.out.println("ITextPDFServiceImpl.generateBiographicalSketchesPDF()");
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		GetProposalResponse proposal = proposalDataServiceClient
+				.getProposal(tempPropId);
+		
+		String filepath = null;
+		
+		List<BioSketches> nrList = new ArrayList<BioSketches>();
+		nrList = proposal.getProposal().getProposalSections().getBioSketches();
+		
+	
+		List<ByteArrayOutputStream> baosList = new ArrayList<ByteArrayOutputStream>();
+		for (BioSketches nr : nrList) {
+			System.out.println(nr.getFilePath());
+			filepath = nr.getFilePath();
+			System.out
+					.println("ITextPDFServiceImpl.generateBiographicalSketchesPDF()..filepath="+filepath);
+			//If file path is not empty , then read PDF from NFS mount
+			if (filepath != null) {	
+				filepath = NFS_MOUNT +filepath;
+				System.out.println("Filepath is ........"+filepath);
+				baosList.add( getPdfFromNfsMount(filepath));	
+				}
+			
+		}
+		
+		if(baosList.size()>0)
+		{
+			return concatentePDF(baosList);
+		}
+		
+		return outputStream;
+	
+	}
+	
+	
+	private ByteArrayOutputStream generateFacEquipmentPDF(String tempPropId) {
+		System.out.println("ITextPDFServiceImpl.generateFacEquipmentPDF()");
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		GetProposalResponse proposal = proposalDataServiceClient
+				.getProposal(tempPropId);
+
+		String filepath = null;
+
+		filepath = proposal.getProposal().getProposalSections()
+				.getFacilitiesEquipmentResources().getFilePath();
+		System.out.println("ITextPDFServiceImpl.generateFacEquipmentPDF()..filepath="+filepath);
+
+		// If file path is not empty , then read PDF from NFS mount
+		if (filepath != null) {
+			filepath = NFS_MOUNT + filepath;
+			System.out.println("Filepath is ........" + filepath);
+			outputStream = getPdfFromNfsMount(filepath);
+
+		}
+
 		return outputStream;
 
 	}
@@ -270,25 +356,28 @@ public class ITextPDFServiceImpl implements PDFService {
 
 	}
 	
-	private ByteArrayOutputStream getPdfFromNfsMount( String filepath) {
-		
-	
+	private ByteArrayOutputStream getPdfFromNfsMount(String filepath) {
+
+		System.out.println("ITextPDFServiceImpl.getPdfFromNfsMount()"
+				+ filepath);
+
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			try {
-				File file = new File(filepath);
+		try {
+			File file = new File(filepath);
 
-				byte b[] = FileCopyUtils.copyToByteArray(file);
+			byte b[] = FileCopyUtils.copyToByteArray(file);
+			System.out.println("*****file length:" + b.length);
 
-				outputStream = new ByteArrayOutputStream(b.length);
+			outputStream = new ByteArrayOutputStream(b.length);
 
-				outputStream.write(b);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			return outputStream;
+			outputStream.write(b);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		return outputStream;
+	}
 	
 	
 
@@ -300,7 +389,7 @@ public class ITextPDFServiceImpl implements PDFService {
 	
 	
 	private ByteArrayOutputStream concatentePDF( List<ByteArrayOutputStream> baosList) {
-		System.out.println("ITextPDFServiceImpl.CreateEntireProposalInternal()");
+		System.out.println("ITextPDFServiceImpl.concatentePDF()....");
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
 		Document document = new Document();
